5aa94a79cd7fdedc08d999fb0b7135c3ccf827b6
bany-ph <bpuertaherrera@gmail.com>
2025-12-06 13:36:23 -0500

    feat: create endpoint GET all and GET by id for ticket

--- src/main/java/com/fixwi/fixwi_backend/infrastructure/adapter/in/web/controller/TicketController.java
+++ src/main/java/com/fixwi/fixwi_backend/infrastructure/adapter/in/web/controller/TicketController.java
@@ -1,11 +1,17 @@
 package com.fixwi.fixwi_backend.infrastructure.adapter.in.web.controller;
 
+import com.fixwi.fixwi_backend.domain.model.Ticket;
 import com.fixwi.fixwi_backend.domain.ports.in.ticket.CreateTicketPort;
+import com.fixwi.fixwi_backend.domain.ports.in.ticket.FindTicketPort;
 import com.fixwi.fixwi_backend.infrastructure.adapter.in.web.dto.request.TicketCreationRequest;
 import com.fixwi.fixwi_backend.infrastructure.adapter.in.web.dto.response.TicketCreationResponse;
 import com.fixwi.fixwi_backend.infrastructure.adapter.in.web.mapper.TicketWebMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 public class TicketController {
 
     private final CreateTicketPort createTicketPort;
+    private final FindTicketPort findTicketPort;
     private final TicketWebMapper ticketWebMapper;
 
     @PostMapping
@@ -30,6 +37,26 @@ public class TicketController {
         var response = ticketWebMapper.toResponse(createdTicket);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+
+    // List all tickets paginated and filter (if needed) by status and category
+    @GetMapping
+    public ResponseEntity<?> getAllTickets(Pageable pageable,
+                                           @RequestParam(required = false)String status,
+                                           @RequestParam(required = false)String category)
+    {
+        Page<Ticket> page = findTicketPort.findAll(pageable, status,category );
+        Page<TicketCreationResponse> response = page.map(ticketWebMapper::toResponse);
+        return ResponseEntity.ok(response);
+    }
+
+
+    //find ticket by id
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        var eventResponse =  ticketWebMapper.toResponse(findTicketPort.findById(id));
+        return ResponseEntity.ok(eventResponse);
     }
 
 }
