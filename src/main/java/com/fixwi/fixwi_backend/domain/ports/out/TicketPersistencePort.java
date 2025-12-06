ae4a2185251e785adeb536c387030c5fe76b81d8
Juan Pablo <juanpablojimenezc@outlook.com>
2025-12-06 17:53:19 -0500

    refactor: add findTicketById to TicketPersistencePort

--- src/main/java/com/fixwi/fixwi_backend/domain/ports/out/TicketPersistencePort.java
+++ src/main/java/com/fixwi/fixwi_backend/domain/ports/out/TicketPersistencePort.java
@@ -2,9 +2,13 @@ package com.fixwi.fixwi_backend.domain.ports.out;
 
 import com.fixwi.fixwi_backend.domain.model.Ticket;
 
+import java.util.Optional;
+
 // puerto de salida, es la persistencia del ticket
 public interface TicketPersistencePort {
 
     // esta linea guarda un ticket nuevo o existente en la bd
     Ticket saveTicket(Ticket ticket);
+
+    Optional<Ticket> findTicketById(Long id);
 }
