7eb486dc72815ca966895ac538a7987bfc71bad6
xXCpFireXx <suarezalejo26@gmail.com>
2025-12-14 13:47:26 -0500

    feat: add id and name to claim JWT token

--- src/main/java/com/fixwi/fixwi_backend/infrastructure/security/adapter/JwtTokenProviderAdapter.java
+++ src/main/java/com/fixwi/fixwi_backend/infrastructure/security/adapter/JwtTokenProviderAdapter.java
@@ -1,8 +1,8 @@
 package com.fixwi.fixwi_backend.infrastructure.security.adapter;
 
-
 import com.fixwi.fixwi_backend.domain.model.User;
 import com.fixwi.fixwi_backend.domain.ports.out.security.TokenProviderPort;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +13,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtTokenProviderAdapter implements TokenProviderPort {
@@ -26,9 +27,12 @@ public class JwtTokenProviderAdapter implements TokenProviderPort {
     @Override
     public String generateToken(User user) {
         Map<String, Object> claims = new HashMap<>();
+        claims.put("id", user.getId());
+        claims.put("fullName", user.getFullName());
         claims.put("role", user.getRole().name());
         claims.put("email", user.getEmail());
 
+        // 2. Construimos el token
         return Jwts.builder()
                 .claims(claims)
                 .subject(user.getEmail())
@@ -38,9 +42,14 @@ public class JwtTokenProviderAdapter implements TokenProviderPort {
                 .compact();
     }
 
+    // --- Métodos de validación y utilidad ---
+
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token);
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
             return true;
         } catch (Exception e) {
             return false;
@@ -48,12 +57,25 @@ public class JwtTokenProviderAdapter implements TokenProviderPort {
     }
 
     public String getUsernameFromToken(String token) {
+        return getClaim(token, Claims::getSubject);
+    }
+
+    public <T> T getSpecificClaim(String token, String claimName, Class<T> type) {
+        final Claims claims = getAllClaims(token);
+        return claims.get(claimName, type);
+    }
+
+    private <T> T getClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaims(String token) {
         return Jwts.parser()
                 .verifyWith(getSigningKey())
                 .build()
                 .parseSignedClaims(token)
-                .getPayload()
-                .getSubject();
+                .getPayload();
     }
 
     private SecretKey getSigningKey() {
