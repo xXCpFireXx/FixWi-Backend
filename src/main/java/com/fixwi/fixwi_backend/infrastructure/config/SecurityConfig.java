2dd8414469351a697b90438fad1d48b890c889f5
jpablo55 <juanpablojimenezc@outlook.com>
2025-12-15 07:52:10 -0500

    fix: enable global CORS support in SecurityConfig
    - Define CorsConfigurationSource bean to allow all origins (*), methods, and headers.
    - Update SecurityFilterChain to apply the new CORS configuration

--- src/main/java/com/fixwi/fixwi_backend/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/fixwi/fixwi_backend/infrastructure/config/SecurityConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
@@ -47,6 +53,25 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    // CORS
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // Permite cualquier origen (Frontend en React, Angular, Postman, etc.)
+        configuration.setAllowedOrigins(List.of("*"));
+
+        // Permite todos los métodos HTTP (GET, POST, PUT, DELETE, OPTIONS, etc.)
+        configuration.setAllowedMethods(List.of("*"));
+
+        // Permite todos los encabezados (Authorization, Content-Type, etc.)
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
